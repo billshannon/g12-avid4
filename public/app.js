feca1933e4238b18667a37adc9f53df4399c30ee
Lisa Carlson <lisa.carlson85@gmail.com>
2015-11-17 22:00:41 -0700

    register page set up

--- public/app.js
+++ public/app.js
@@ -2,11 +2,18 @@ angular.module('MyApp', ['ngRoute'])
   .controller('LandingController', ['$scope', function ($scope) {
 
   }])
+  .controller('RegisterController', ['$scope', function ($scope) {
+
+  }])
   .config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: '/partials/landing.html',
         controller: 'LandingController'
+      })
+      .when('/register', {
+        templateUrl: '/partials/register.html',
+        controller: 'RegisterController'
       })
       .when('/page-not-found', {
         templateUrl: '/partials/error.html'
