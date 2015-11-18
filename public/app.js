a28785ff056dbdb9ebae187c80307e3b03b41c17
Bill Shannon <bill@billshannon.com>
2015-11-18 15:39:19 -0700

    add activity page with sample activiites

--- public/app.js
+++ public/app.js
@@ -1,26 +1,30 @@
-angular.module('MyApp', ['ngAnimate','ngRoute'])
-  .controller('LandingController', ['$scope', function ($scope) {
+angular.module('MyApp', ['ngAnimate', 'ngRoute'])
+    .controller('LandingController', ['$scope', function ($scope) {
 
-  }])
-  .controller('RegisterController', ['$scope', function ($scope) {
+    }])
+    .controller('RegisterController', ['$scope', function ($scope) {
 
-  }])
-  .config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: '/partials/landing.html',
-        controller: 'LandingController'
-      })
-      .when('/register', {
-        templateUrl: '/partials/register.html',
-        controller: 'RegisterController'
-      })
-      .when('/page-not-found', {
-        templateUrl: '/partials/error.html'
-      })
-      .otherwise({
-        redirectTo: '/page-not-found'
-      });
-    $locationProvider.html5Mode(true);
-  }]);
+    }])
+    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '/partials/landing.html',
+                controller: 'LandingController'
+            })
+            .when('/register', {
+                templateUrl: '/partials/register.html',
+                controller: 'RegisterController'
+            })
+            .when('/activities', {
+                templateUrl: '/partials/activities.html',
+                controller: 'ActivitiesController'
+            })
+            .when('/page-not-found', {
+                templateUrl: '/partials/error.html'
+            })
+            .otherwise({
+                redirectTo: '/page-not-found'
+            });
+        $locationProvider.html5Mode(true);
+    }]);
   
